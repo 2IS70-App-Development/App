507e2a9fa7d3d1c441952018a005208b88739e76
Amir Nurmukhambetov <thereug6@gmail.com>
2026-04-01 00:12:38 +0200

    update SonarQube configuration and add project dependencies
    - Move SonarQube configuration to the root `build.gradle.kts` and update version.
    - Add and organize dependencies in `libs.versions.toml`, including Navigation, CameraX, ML Kit, Security Crypto, and ZXing.
    - Refactor JaCoCo test report task in `app/build.gradle.kts` to integrate unit and instrumented test coverage.
    - Simplify repository declarations in `settings.gradle.kts`.
    
    Signed-off-by: Amir Nurmukhambetov <thereug6@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.sonar)
-    jacoco
+    id("jacoco") //Enable the JaCoCo Plugin
+}
+
+jacoco {
+    toolVersion = "0.8.14" //Defines which JaCoCo version will be used
 }
 
 android {
@@ -35,7 +38,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true
     }
     buildFeatures {
         compose = true
@@ -50,82 +52,6 @@ android {
     }
 }
 
-sonar {
-    properties {
-        property("sonar.projectName", "CryptoSeal")
-        property("sonar.projectKey", "2IS70-App-Development_App")
-        property("sonar.organization", "2is70-app-development")
-        property("sonar.host.url", "https://sonarcloud.io")
-        
-        // Point to the JaCoCo XML report (Relative to app folder)
-        property("sonar.coverage.jacoco.xmlReportPaths", "${project.buildDir}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
-        
-        // Exclude UI from analysis
-        property("sonar.exclusions", "**/R.java, **/BuildConfig.java, **/*Activity*.kt, **/*Tab*.kt, **/*Screen*.kt, **/*Composable*.kt, **/*Preview*.kt, **/Theme*.kt, **/Navigation*.kt")
-    }
-}
-
-val fileFilter = mutableSetOf(
-    "**/R.class",
-    "**/R$*.class",
-    "**/BuildConfig.*",
-    "**/Manifest*.*",
-    "**/*Test*.*",
-    "android/**/*.*",
-    "**/*Activity*.*",
-    "**/*TabKt*.*",
-    "**/*ScreenKt*.*",
-    "**/*ComposableKt*.*",
-    "**/*PreviewKt*.*",
-    "**/ThemeKt*.*",
-    "**/NavigationKt*.*",
-    "**/*Application*.*"
-)
-
-tasks.withType<Test> {
-    configure<JacocoTaskExtension> {
-        isIncludeNoLocationClasses = true
-        excludes = listOf("jdk.internal.*")
-    }
-}
-
-project.afterEvaluate {
-    tasks.register<JacocoReport>("jacocoTestReport") {
-        dependsOn("testDebugUnitTest")
-        group = "Reporting"
-        description = "Generate Jacoco coverage reports"
-
-        reports {
-            xml.required.set(true)
-            html.required.set(true)
-            xml.outputLocation.set(file("${project.buildDir}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"))
-            html.outputLocation.set(file("${project.buildDir}/reports/jacoco/jacocoTestReport/html"))
-        }
-
-        val javaClasses = fileTree("${project.buildDir}/intermediates/javac/debug") {
-            include("**/*.class")
-            exclude(fileFilter)
-        }
-        val kotlinClasses = fileTree("${project.buildDir}/tmp/kotlin-classes/debug") {
-            include("**/*.class")
-            exclude(fileFilter)
-        }
-        classDirectories.setFrom(files(javaClasses, kotlinClasses))
-
-        sourceDirectories.setFrom(files(
-            "${project.projectDir}/src/main/java",
-            "${project.projectDir}/src/main/kotlin"
-        ))
-
-        executionData.setFrom(fileTree(project.buildDir) {
-            include(
-                "outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec",
-                "outputs/code_coverage/debugAndroidTest/connected/*coverage.ec"
-            )
-        })
-    }
-}
-
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -135,31 +61,76 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.material.icons.extended)
+
+    implementation(libs.gson)
+    implementation(libs.androidx.security.crypto)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.barcode.scanning)
+    implementation(libs.play.services.location)
+    implementation(libs.zxing.core)
 
     testImplementation(libs.junit)
-    testImplementation(libs.mockk)
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
-    
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    androidTestImplementation(libs.mockk.android)
-    
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
 
-    implementation("androidx.navigation:navigation-compose:2.9.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.camera:camera-camera2:1.5.3")
-    implementation("androidx.camera:camera-lifecycle:1.5.3")
-    implementation("androidx.camera:camera-view:1.5.3")
-    implementation("androidx.camera:camera-core:1.5.3")
-    implementation("com.google.mlkit:barcode-scanning:17.3.0")
-    implementation("com.google.android.gms:play-services-location:21.3.0")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.zxing:core:3.5.3")
-    implementation("androidx.security:security-crypto:1.1.0")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+// Create a custom JaCoCo report task
+tasks.register<JacocoReport>("jacocoTestReport") {
+
+    // Run BOTH test types before generating report
+    dependsOn("testDebugUnitTest", "connectedDebugAndroidTest")
+
+    //Configure output formats
+    reports {
+        xml.required.set(true) // XML -> required for SonarQube
+        html.required.set(true) //HTML -> visual coverage report
+    }
+
+    // Exclude auto-generated or irrelevant files from coverage
+    val fileFilter = listOf(
+        "**/R.class", // Android resources classes
+        "**/R$*.class", // Nested resources classes
+        "**/BuildConfig.*", // Auto-generated BuildConfig
+        "**/Manifest*.*", // Manifest-generated files
+        "**/*Test*.*", // Exclude test classes
+        "android/**/*.*" //Android framework classes
+    )
+
+    //Kotlin compiled classes for debug build
+    val kotlinClasses = fileTree(layout.buildDirectory.dir("tmp/kotlin-classes/debug")) {
+        exclude(fileFilter)
+    }
+
+    //Java compiled classes for debug build
+    val javaClasses = fileTree(layout.buildDirectory.dir("intermediates/javac/debug")) {
+        exclude(fileFilter)
+    }
+
+    // Compiled Kotlin + Java class directories
+    classDirectories.setFrom(files(kotlinClasses, javaClasses))
+
+    // Tell JacoCo where your actual source code lives
+    sourceDirectories.setFrom(files("src/main/java", "src/main/kotlin"))
+
+    //Point to the JaCoCo execution data generated by unit tests and instrumented tests
+    executionData.setFrom(fileTree(layout.buildDirectory) {
+
+        include(
+            // Unit test coverage
+            "outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec",
+
+            // Instrumented test coverage
+            "outputs/code_coverage/debugAndroidTest/connected/**/*.ec"
+        )
+    }
+    )
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,49 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.sonar) apply false
+    id("org.sonarqube") version "7.1.0.6387"
+}
+
+sonar {
+    properties {
+        property(
+            "sonar.projectKey",
+            "2IS70-App-Development_App"
+        ) // Unique project identifier in SonarQube
+        property("sonar.projectName", "2is70-app-development") // Display name shown in SonarQube UI
+        property("sonar.host.url", "https://sonarcloud.io") // SonarQube server URL
+        property("sonar.token", "1f5a057acb8a6578795596b112cc23789f53cb82") // Authentication token
+        property(
+            "sonar.sources",
+            "app/src/main/java,app/src/main/kotlin"
+        ) // Application source code paths
+        property("sonar.tests", "app/src/test/java,app/src/test/kotlin") // Unit test source paths
+        property(
+            "sonar.test.inclusions",
+            "**/*Test*.kt, **/*Test*.java"
+        )  // Identifies test classes
+        property(
+            "sonar.java.binaries",
+            "app/build/tmp/kotlin-classes/debug,app/build/intermediates/javac/debug"
+        ) // Compiled bytecode required for static analysis
+        property(
+            "sonar.junit.reportPaths",
+            "app/build/test-results/testDebugUnitTest"
+        )  // JUnit test execution reports
+        property(
+            "sonar.exclusions",
+            "**/R.class, **/R$*.class, **/BuildConfig.*, **/Manifest*.*, build/**"
+        )  // Exclude generated and irrelevant files
+
+        property(
+            "sonar.scm.disabled",
+            "true"
+        ) // Temporarily disable SCM detection to avoid Git autodetection error
+
+        //Integrate JacoCo to SonarQube
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${rootProject.projectDir}/app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
+        )
+    }
 }
